a728f017927e40346c9ec891a1a4901ae7c82bf9
locho0111 <rickho@Ricks-MacBook-Pro.local>
2021-04-10 18:06:22 -0500

    Rick's 1st commit

--- app.py
+++ app.py
@@ -5,26 +5,65 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
 import os
+import json
 
-client_id = os.getenv('CLIENT_ID')
-client_secret = os.getenv('CLIENT_SECRET')
+client_id = "2299de3db00442eba2d6bdb9bb9b0127"
+client_secret = "6b78cccaacae4a52809b1f49ecd6c9dc"
 
 client_credentials_manager = SpotifyClientCredentials(client_id, client_secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 
+def getTrackFeatures(id):
+	meta = sp.track(id)
+	features = sp.audio_features(id)
+
+	# meta
+	name = meta['name']
+	album = meta['album']['name']
+	artist = meta['album']['artists'][0]['name']
+	release_date = meta['album']['release_date']
+	length = meta['duration_ms']
+	popularity = meta['popularity']
+
+	# features
+	acousticness = features[0]['acousticness']
+	danceability = features[0]['danceability']
+	energy = features[0]['energy']
+	instrumentalness = features[0]['instrumentalness']
+	liveness = features[0]['liveness']
+	loudness = features[0]['loudness']
+	speechiness = features[0]['speechiness']
+	tempo = features[0]['tempo']
+	time_signature = features[0]['time_signature']
+
+	track = [name, album, artist, release_date, length, popularity, danceability, acousticness, danceability, energy, instrumentalness, liveness, loudness, speechiness, tempo, time_signature]
+	return track
+
 @app.route('/',methods = ['POST', 'GET'])
 def login():
 	if request.method == 'POST':
-		search_text = request.form['song_name']
-
-		results = sp.search(q=search_text, limit=10)
-		songlist = results['tracks']['items']
-
-		return render_template('home.html', tracks=songlist)
-	else:
+		song_name = request.form['song_name']
+		artist_name = request.form['artist_name']
+		#print(song_name)
+		#print(artist_name)
+		results = sp.search(q='artist:' + artist_name + ' track:' + song_name, limit=1, type='track')
+		# for idx, track in enumerate(results['tracks']['items']):
+		# 	print(idx, track['name'])
+		#print(results)
+		items = results['tracks']['items']
+		#print(json.dumps(items, indent= 4))
+		song_uri = items[0]['uri']
+		track_info = getTrackFeatures(song_uri)
+		print(track_info)
+		return render_template('spotify-flask.html', tracks=track_info)
+		# return jsonify(results)
+	else: # GET
 		user = request.args.get('song_name')
-		return render_template('home.html')
+		return render_template('index.html')
+
+
+
 
 
 if __name__ == '__main__':
